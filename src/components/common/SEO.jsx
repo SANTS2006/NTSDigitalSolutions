f86468eb302c71e4b6dd4add26cfdb4a51bea1cc
Newton Tamba Sam <newtonsam602@gmail.com>
2026-07-22 20:17:34 +0000

    Add Open Graph social sharing metadata

--- src/components/common/SEO.jsx
+++ src/components/common/SEO.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 
 
-function SEO({title = "NTS Digital Solutions | Empowering Businesses Through Digital Innovation", description = "NTS Digital Solutions builds custom digital solutions that help businesses grow."}) {
+function SEO({title = "NTS Digital Solutions | Empowering Businesses Through Digital Innovation", description = "NTS Digital Solutions builds custom digital solutions that help businesses grow.", url = "https://ntsdigitalsolutions.tech", image = "/logo.png", }) {
 
     return (
 
@@ -15,6 +15,86 @@ function SEO({title = "NTS Digital Solutions | Empowering Businesses Through Dig
 
                 content={description}
             />
+
+            <meta 
+                property="og: title"
+                content={title}
+            />
+
+            <meta
+
+                property="og:description"
+
+                content={description}
+
+            />
+
+
+            <meta
+
+                property="og:image"
+
+                content={image}
+
+            />
+
+
+            <meta
+
+                property="og:url"
+
+                content={url}
+
+            />
+
+
+            <meta
+
+                property="og:type"
+
+                content="website"
+
+            />
+
+
+
+            {/* Twitter */}
+
+            <meta
+
+                name="twitter:card"
+
+                content="summary_large_image"
+
+            />
+
+
+            <meta
+
+                name="twitter:title"
+
+                content={title}
+
+            />
+
+
+            <meta
+
+                name="twitter:description"
+
+                content={description}
+
+            />
+
+
+            <meta
+
+                name="twitter:image"
+
+                content={image}
+
+            />
+
 
         </Helmet>
 
