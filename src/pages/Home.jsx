b59ba9024f4b4bec4acbcfca3222893e1bdbe456
Newton Tamba Sam <newtonsam602@gmail.com>
2026-07-22 20:10:13 +0000

    Add SEO foundation with React Helmet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,12 @@ import IndustriesPreview from "../components/sections/IndustriesPreview";
 import WhyChoose from "../components/sections/WhyChoose";
 import Founder from "../components/sections/Founder";
 import CTA from "../components/sections/CTA";
+import SEO from "../components/common/SEO";
 
 function Home() {
     return (
         <>
+           <SEO title="NTS Digital Solutions | Empowering Businesses Through Digital Innovation" description="NTS Digital Solutions provides custom software development, web applications, and digital solutions that help businesses and organizations grow." />
             <Hero />
             <ServicesPreview />
             <SolutionsPreview />
